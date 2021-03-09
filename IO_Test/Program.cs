73746130940188c4be24818008b558c0a922e6a9
bartoszluka <bartoszluka1@gmail.com>
2021-03-09 16:23:02 +0100

    basic int parse

--- IO_Test/Program.cs
+++ IO_Test/Program.cs
@@ -6,17 +6,19 @@ using System.Threading.Tasks;
 
 namespace IO_Test
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
-    public class Calculator
-    {
-        public static int  Calculate(string s)
-        {
-            return 0;
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+	public class Calculator
+	{
+		public static int Calculate(string s)
+		{
+			int n = int.Parse(s);
+
+			return n;
+		}
+	}
 }
