212a4547a6c47c6a0a4ca209d93a962c36dcba75
bartoszluka <bartoszluka1@gmail.com>
2021-03-09 16:28:09 +0100

    created new branch

--- IO_Test/Program.cs
+++ IO_Test/Program.cs
@@ -6,19 +6,19 @@ using System.Threading.Tasks;
 
 namespace IO_Test
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-		}
-	}
-	public class Calculator
-	{
-		public static int Calculate(string s)
-		{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Calculator
+    {
+        public static int  Calculate(string s)
+        {
 			int n = int.Parse(s);
 
 			return n;
-		}
-	}
+        }
+    }
 }
